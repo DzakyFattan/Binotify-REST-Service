e502cbea599aed216fca34b915b52883ad5c5da2
wmk567 <13520020@std.stei.itb.ac.id>
2022-11-30 15:11:38 +0700

    nambah cors

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors')
 const routes = require('./src/routes');
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('/api to use the api');
