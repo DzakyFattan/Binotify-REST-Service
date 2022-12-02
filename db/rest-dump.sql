60d6fd862e13bec447c8208d9500190af292fc3b
dzaky.fattan <70198829+DzakyFattan@users.noreply.github.com>
2022-12-02 08:28:15 +0700

    populate db

--- db/rest-dump.sql
+++ db/rest-dump.sql
@@ -16,4 +16,12 @@ create table if not exists binotify_songs (
         foreign key (id_penyanyi) 
         references binotify_user(id_user)
         on delete cascade
-)
+);
+
+INSERT INTO public.binotify_songs VALUES (1, 'Asu no Yozora Shoukaihan', 1, 'song/1669943771587-03. アスノヨゾラ哨戒班.mp3');
+INSERT INTO public.binotify_songs VALUES (2, 'Lost One''s Weeping', 1, 'song/1669943827964-04. ロストワンの号哭.mp3');
+INSERT INTO public.binotify_songs VALUES (3, 'Usseewa', 2, 'song/1669944003771-11.うっせぇわ.mp3');
+INSERT INTO public.binotify_songs VALUES (4, 'Yoru no Piero', 2, 'song/1669944071996-14.夜のピエロ.mp3');
+
+INSERT INTO public.binotify_user VALUES (1, 'afterglow@bando.ri', 'U2FsdGVkX1+FSn6n9irv/qLCQ27Wqd7R6hjlWQRjfYM=', 'Afterglow', 'Mitake Ran', false);
+INSERT INTO public.binotify_user VALUES (2, 'ado@ado.do', 'U2FsdGVkX1/k7M+HBOTHpiJUJw6VHSPvBiTLgO6rxR4=', 'AdoAdoAdo', 'AdoAdoAdo', false);
